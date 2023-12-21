a07a3e3bf96307eb2a66c05ac2ee886381f88e90
Shashwat Gupta <shashwatgupta21054@gmail.com>
2023-12-21 18:54:03 +0530

    some changes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import StudentDetail from './StudentDetail';
-import { useState } from 'react';
+
 import StudentHome from './StudentHome';
 function App() {
-  const [show, setShow] = useState(false);
-  
-  const buttonHandler = () => {
-
-    setShow(!show);
-  }
+ 
   return (
     <Routes>
       <Route path='/' element={<StudentHome/>}/>
